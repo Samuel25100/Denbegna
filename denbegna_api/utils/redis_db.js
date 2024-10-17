4651fff9b2dff046d3e5d67a9e80b9144a049334
Samuel25100 <samuelgselassie1@gmail.com>
2024-10-17 10:51:41 +0300

    fixing redis connection problem

--- denbegna_api/utils/redis_db.js
+++ denbegna_api/utils/redis_db.js
@@ -1,13 +1,10 @@
 import { createClient } from 'redis';
 import dotenv from 'dotenv';
 
-dotenv.config();
-
-
 class RedisClient {
 	constructor() {
 		this.client = createClient({
-			url: process.env.REDIS_URL
+			url: 'redis://127.0.0.1:6379'
 		});
 		this.ready = true;
 		this.client.on('error', (err) => {
